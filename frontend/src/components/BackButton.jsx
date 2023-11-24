8dfa6edc8cb1198dc5ae5cf3fd51cb965745456f
brodie <brodie.jung@gmail.com>
2023-11-24 03:52:53 -0800

    added frontend and refactored backend

---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import {BsArrowLeft} from 'react-icons/bs';
+
+const BackButton = ({destination = '/'}) => {
+  return (
+    <div>BackButton</div>
+  )
+}
+
+export default BackButton
