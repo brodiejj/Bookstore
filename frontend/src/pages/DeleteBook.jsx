82ca5f3ada92e54293e5a6de95864f24ab62b872
brodie <brodie.jung@gmail.com>
2023-11-24 17:07:08 -0800

    added show book page

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import {Link} from 'react-router-dom';
 
 const DeleteBook = () => {
   return (
