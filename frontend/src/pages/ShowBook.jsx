a41c9055e50dc7dde8476888c2580a4aa591d91a
brodie <brodie.jung@gmail.com>
2023-11-27 20:55:48 -0800

    Added delete and edit functionality. Added alternative view for homepage

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
 const ShowBook = () => {
   const [book, setBook] = useState({});
   const [loading, setLoading] = useState(false);
-  const {id} = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     setLoading(true);
@@ -25,12 +25,12 @@ const ShowBook = () => {
 
   return (
     <div className='p-4'>
-      <BackButton/>
+      <BackButton />
       <h1 className='text-3xl my-4'>Show Book</h1>
 
       {loading ? (
-        <Spinner/>
-      ):(
+        <Spinner />
+      ) : (
         <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
           <div className='my-4'>
             <span className='text-xl mr-4 text-gray-500'>Id</span>
