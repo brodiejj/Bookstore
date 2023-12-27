7024289cc999afcab81dc0cc42d5e97e5fccd80f
brodie <brodie.jung@gmail.com>
2023-12-26 17:47:04 -0800

    Added Modal components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
-import {Link} from 'react-router-dom';
-import {AiOutlineEdit} from 'react-icons/ai';
-import {BsInfoCircle} from 'react-icons/bs';
-import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 import BooksCard from '../components/home/BooksCard';
 import BooksTable from '../components/home/BooksTable';
 import { Book } from '../../../backend/models/bookModel';
@@ -36,15 +36,15 @@ const Home = () => {
       <div className='flex justify-between item-center'>
         <h1 className='text-3xl my-8'>Books List</h1>
         <Link to='/books/create'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl'/>
+          <MdOutlineAddBox className='text-sky-800 text-4xl' />
         </Link>
       </div>
       {loading ? (
-        <Spinner/>
+        <Spinner />
       ) : showType == 'table' ? (
-        <BooksTable books={books}/>
+        <BooksTable books={books} />
       ) : (
-        <BooksCard books={books}/>
+        <BooksCard books={books} />
       )}
     </div>
   )
